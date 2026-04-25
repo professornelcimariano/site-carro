350b49fb51fa8c90831a87150996f63de5f0b5ae
Professor Nelci Mariano <professornelcimariano@gmail.com>
2026-04-24 22:11:54 -0300

    Commit Inicial do Projeto

---
+++ produtos.js
@@ -0,0 +1,58 @@
+const produtos = [
+    {
+        id: 1,
+        name: "Carro de Luxo",
+        description: " Carro de Luxo com design elegante e desempenho excepcional, motor potente, interior sofisticado e tecnologia avançada para uma experiência de condução premium.",
+        price: 50000,
+        image: "images/car/car-1.avif" // Caminho para sua pasta local
+    },
+    {
+        id: 2,
+        name: "Carro Esportivo Vermelho",
+        description: "Carro Esportivo Vermelho com design agressivo e desempenho excepcional, motor potente, interior esportivo e tecnologia avançada para uma experiência de condução emocionante.",
+        price: 30000,
+        image: "images/car/car-2.avif"
+    },
+    {
+        id: 3,
+        name: "Carro SUV Preto",        
+        description: "Carro SUV Preto com design robusto e versátil, motor potente, interior espaçoso e tecnologia avançada para uma experiência de condução confortável e segura em qualquer terreno.",
+        price: 40000,
+        image: "images/car/car-3.avif"
+    },
+    {
+        id: 4,
+        name: "Carro 4 X 4 Cinza",
+        description: "Carro 4 X 4 Cinza com design robusto e versátil, motor potente, interior espaçoso e tecnologia avançada para uma experiência de condução confortável e segura em qualquer terreno.",
+        price: 20000,
+        image: "images/car/car-4.avif"
+    }, 
+    {
+        id: 5,
+        name: "Carro de luxo Preto",
+        description: "Carro de luxo Preto com design elegante e desempenho excepcional, motor potente, interior sofisticado e tecnologia avançada para uma experiência de condução premium.",
+        price: 350000,
+        image: "images/car/car-5.avif"
+    }, 
+    {
+        id: 6,
+        name: "Carro Esportivo Prata",
+        description: "Carro Esportivo Prata com design agressivo e desempenho excepcional, motor potente, interior esportivo e tecnologia avançada para uma experiência de condução emocionante.",
+        price: 300000,
+        image: "images/car/car-6.avif"
+    }, 
+    {
+        id: 7,
+        name: "Carro Esportivo Branco/Prata",
+        description: "Carro Esportivo Branco/Prata com design agressivo e desempenho excepcional, motor potente, interior esportivo e tecnologia avançada para uma experiência de condução emocionante.",
+        price: 300000,
+        image: "images/car/car-7.avif"
+    }, 
+    {
+        id: 8,
+        name: "Carro Porsche Branco",
+        description: "Carro Porsche Branco com design elegante e desempenho excepcional, motor potente, interior sofisticado e tecnologia avançada para uma experiência de condução premium.",
+        price: 500000,
+        image: "images/car/car-8.avif"
+    }
+];
